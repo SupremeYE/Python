6dbcf03a1a5300f50604c7632cbde86cd04a708f
SupremeYE <nina7963@naver.com>
2020-02-16 16:59:27 +0900

    input()을 사용하여 값을 입력하고 type()를 사용하여 변수의 타입을 알수있다.

---
+++ 1.py
@@ -0,0 +1,16 @@
+'''a=input()
+print(a)
+print(type(a))'''
+
+'''a=input()
+b=input()
+type("a type:"+a)
+type("b type:"+b)
+print(a+b)'''
+
+'''a=input("a input value:")
+b=input("b input value:")
+sum_str=a+"+"+b
+print(eval(sum_str))'''
+
+

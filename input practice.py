eb0e13ac287dc192ef2765d1e7bf4bbd06e6cec6
SupremeYE <nina7963@naver.com>
2020-02-17 12:56:52 +0900

    가위바위보 input()

---
+++ input practice.py
@@ -0,0 +1,28 @@
+'''a=input()
+print(a)
+print(type(a))'''
+
+'''a=input()
+b=input()
+type("a type:"+a)
+type("b type:"+b)
+print(a+b)'''
+
+'''a=input("a input value:")
+b=input("b input value:")
+sum_str=a+"+"+b
+print(eval(sum_str))'''
+
+
+''''#입력받기1
+print('가위 바위 보 가운데 하나를 내 주세요', end=' ')
+mine =input()
+print('mine',mine)
+
+
+#입력받기2
+mine =input('가위 바위 보 가운데 하나를 내 주세요')
+print('mine',mine)'''
+
+print(round(4.5))
+print(round(3.5))
